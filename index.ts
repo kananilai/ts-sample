af008939c0d965e80dcf1845911d9823fd95bfe8
kananilai <kana.nilai4646@gmail.com>
2022-07-20 15:02:23 +0900

    ✨ non-null-assertion

--- index.ts
+++ index.ts
@@ -1,13 +1,9 @@
-//リテラルタイプスで推論されている
-export const color = "red";
+export function getFirstLatter(str?: string) {
+  //non-null-assertion、無理やり通す
+  // return str!.charAt(0);
+  if (!str) {
+    return;
+  }
+  return str.charAt(0);
+}
 
-// string Literal Typesにダウンキャスト
-// redしか受け取らない
-// 自分が指定した値であることを保証する
-
-const theme = {
-  // color: "red" as "red",
-  // const assertion⏬
-  color: "red",
-  backgroundColor: "blue",
-} as const;
