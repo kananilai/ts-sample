877f913c081b4f3173da22dc1d0c185b63b4a908
kananilai <kana.nilai4646@gmail.com>
2022-08-02 21:02:56 +0900

    #17 型ガード

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
-//double assertion
-export function getFirstLatter(str:number) {
-  //as unknown でアップキャストして、as stringでダウンキャスト
-  return (str as unknown as string).charAt(0);
-}
+type UserA = { name: string; lang: "ja" };
+type UserB = { name: string; lang: "en" };
+type UserC = { name: string; lang: "fr" };
 
+export const foo = (value: UserA | UserB | UserC) => {
+  switch (value.lang) {
+    case "ja": {
+      return value;
+    }
+    case "en": {
+      return value;
+    }
+    case "fr": {
+      return value;
+    }
+    default: {
+      throw new Error("lang is not defined!");
+    }
+  }
+  // if (value.lang === "ja") {
+  //   return value;
+  // }
+  // return value;
+};
