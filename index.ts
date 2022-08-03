a309e5e297961041ba7975e21fa14cae09c4eea3
kananilai <kana.nilai4646@gmail.com>
2022-08-03 10:39:12 +0900

    #19 関数のGenerics

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
-//<T = string>で初期値設定
-//extendsはGenericsとはいえある程度制限したい時に使う
-export type Foo<T extends string | number = string> = {
-  value: T;
+//Genericsは引数の前に記述
+const foo = <T extends string>(arg: T) => {
+  //extendsで型を指定することで、メソッドが使える様になる。extendsで型制限をしないとunknown型になる。
+  //ユニオンタイプスの場合は、メソッドにアクセスできない⇨if文で型を絞る必要がある。(typeofなど)
+  return { value: arg };
 };
 
-const foo1: Foo = {
-  value: "",
-};
-const foo2: Foo<number> = {
-  value: 222,
-};
+// const foo1 = foo("");
+// const foo2 = foo(1);
+// const foo3 = foo([true]);
