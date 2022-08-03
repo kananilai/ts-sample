1274c6485daf778f1ad4e20b77d06ba1290ea85a
kananilai <kana.nilai4646@gmail.com>
2022-08-03 14:52:55 +0900

    #22 Utility Types

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
-const foo = (id: string, age: number, name: string) => {
-  return 0;
+type User = {
+  name: string;
+  age: number | null;
+  country?: "US" | "UK" | "JP";
 };
 
-//今回は返り値の方をnumberにしているから=>で指定している。なんでも良い時はanyを使う。
-type Return<T> = T extends (...args: [any, infer U, ...any[]]) => any
-  ? U
-  : never;
+type OmitUser = Omit<User,"age">;
 
-type Foo = Return<typeof foo>;
+const user: OmitUser = {
+  name: "John",
+  // age: 20,
+  country: "US",
+};
+
