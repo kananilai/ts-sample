b13e9e2b3ae157073a168e121b24fe00c9d98b37
kananilai <kana.nilai4646@gmail.com>
2022-08-03 15:18:26 +0900

    #23 Utility types外部パッケージ

--- index.ts
+++ index.ts
@@ -1,14 +1,50 @@
+import { PartialDeep } from "type-fest";
+
+//互換性があるものが残る
+type Foo1 = Extract<string | number, string>;
+
+//互換性がないものが残る
+type Foo2 = Exclude<string | number, string>;
+
+//nullとundefinedを除外する
+type Foo3 = NonNullable<string | null | undefined>;
+
+//objectのkeyとvalueを指定
+type Foo4 = Record<string, number>;
+
+const obj: Foo4 = {
+  hoge: 1,
+  huge: 2,
+};
+
+//関数の引数をTupleとして取得
+function foo(a: string, b: number[], c: boolean) {
+  return;
+}
+type Foo5 = Parameters<typeof foo>;
+
+//文字列操作、組み込みのUtility Types
+type Foo6 = Uppercase<"hello">;
+type Foo7 = Lowercase<"Hello">;
+type Foo8 = Capitalize<"hello">;
+type Foo9 = Uncapitalize<"Hello">;
+
+
+//type fest
+
+
+
 type User = {
   name: string;
   age: number | null;
-  country?: "US" | "UK" | "JP";
+  address:{
+    country: "US" | "UK" | "JP"
+  }
 };
+//o@tionalにする。標準のPartialではネストされたプロパティには対応していない 👉partialDeepで対応
+type PartialUser = PartialDeep<User>;
 
-type OmitUser = Omit<User,"age">;
-
-const user: OmitUser = {
+const user: PartialUser = {
   name: "John",
-  // age: 20,
-  country: "US",
+  address: {},
 };
-
